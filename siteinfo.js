d4a21df50fa4d2342030ba559663def50cb4e93f
没有蜡 <zhengyq93@163.com>
2020-07-01 09:59:28 +0800

    no commit message

--- siteinfo.js
+++ siteinfo.js
@@ -4,5 +4,5 @@
 module.exports = {
   name: "zyq小程序商城",
   // siteroot :'http://yoshop.cn/',
-  siteroot: "http://39.105.5.52/", // 必填: api地址，结尾要带/
+  siteroot: "https://www.chengyi.xyz/miniapp/", // 必填: api地址，结尾要带/
 };
